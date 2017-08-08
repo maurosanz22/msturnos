50c63544485b5fb2a3b8eb67ec8639c80bfeee92
Lucio G. Moya <luciomoya94@gmail.com>
2017-08-07 22:13:21 -0300

    R2-Lucio : Generacion de turnos en lote

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -7,6 +7,9 @@ class ShiftsController < ApplicationController
     @activities = current_user.get_company_user_admin.get_all_activities 
   end
 
+  def index
+  end
+=begin
   def create
     @activity = Activity.find(params[:activity_id])
     @shift = Shift.new(shift_params)
@@ -23,14 +26,70 @@ class ShiftsController < ApplicationController
       end
     end
   end
+=end
+  def create
+    @activity = Activity.find(params[:activity_id])
+    start_date = Date.parse(params[:fecha_d])
+    end_date = Date.parse(params[:fecha_h])
+    hora_inicio = Time.parse(params[:hora_inicio])
+    hora_fin = Time.parse(params[:hora_fin])
+    set_days
+
+    (start_date..end_date).each do |day|
+      if @dias.include? day.wday
+        shift = Shift.new
+        shift.fecha = day
+        shift.hora_inicio = hora_inicio
+        shift.hora_fin = hora_fin
+        shift.activity_id = @activity.id
+
+        shift.save
+      end
+=begin
+      shift = Shift.newsave
+      shift.fecha = day
+      shift.hora_inicio = hora_inicio
+      shift.hora_fin = hora_fin
+      shift.activity_id = @activity.id
+
+      shift.
+=end
+    end
+  end
 
   private
 
   def shift_params
-    params.permit(:fecha, :hora_inicio, :hora_fin)
+    params.permit(:fecha_d, :fecha_h, :hora_inicio, :hora_fin, :lunes_c, :martes_c, :miercoles_c, :jueves_c, :viernes_c, :sabado_c, :domingo_c)
   end
 
   def set_branches
     @branches = current_user.get_company_user_admin.get_all_branchs
+  end
+
+  def set_days
+    @dias = []
+
+    if params[:lunes_c]
+      @dias.push(1)
+    end
+    if params[:martes_c]
+      @dias.push(2)
+    end
+    if params[:miercoles_c]
+      @dias.push(3)
+    end
+    if params[:jueves_c]
+      @dias.push(4)
+    end
+    if params[:viernes_c]
+      @dias.push(5)
+    end
+    if params[:sabado_c]
+      @dias.push(6)
+    end
+    if params[:domingo_c]
+      @dias.push(0)
+    end
   end
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,29 @@
 class Shift < ApplicationRecord
   belongs_to :activity
+
+  def get_all_branchs
+    branches.all
+  end
+
+  def get_all_activities
+    activities = []
+    get_all_branchs.each do |b|
+      b.activities.all.each do |a|
+        activities << a
+      end
+    end
+
+    return activities
+  end
+
+  def get_all_shifts
+  	shifts = []
+    get_all_activities.each do |b|
+      b.shifts.all.each do |a|
+        shifts << a
+      end
+    end
+
+    return shifts
+  end
 end
